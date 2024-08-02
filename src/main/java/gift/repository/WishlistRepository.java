3c81f68272904a974a311c011593ca7dcecf303f
kuma4188 <psb4188@gmail.com>
2024-08-02 11:51:05 +0900

    step2 완료

--- src/main/java/gift/repository/WishlistRepository.java
+++ src/main/java/gift/repository/WishlistRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
     Page<Wishlist> findBySiteUserUsernameAndHiddenFalseOrderByCreatedDateDesc(String username, Pageable pageable);
+
 }
